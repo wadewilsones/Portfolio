a27da1f4287563aee8459182bfaff85456c68ed5
Vlada <70239279+wadewilsones@users.noreply.github.com>
2022-07-20 14:04:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,6 @@ function displayData (){
     container.innerHTML = "<p>"+text+"</p>";
 }
 
-let text = 'Hello, my name is Vlada. I am a motivated self-taught front-end developer. I have +1.5 years of experience with technologies like HTML, CSS, JavaScript, React, and Node. Recently created and deployed my own application using the MERN stack. Currently learning Python and Flask for back-end!'
-
 function displayPic (){
     container.innerHTML = '';
 }
